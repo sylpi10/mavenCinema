fcdce67e24204fd170e5a74e27cd114a37d3b8cc
syl pi <syl.pillet@hotmail.fr>
2020-01-20 11:28:21 +0100

    create testSelectAll

--- src/test/java/cinema/persistence/entity/test/TestMovie.java
+++ src/test/java/cinema/persistence/entity/test/TestMovie.java
@@ -2,6 +2,10 @@ package cinema.persistence.entity.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -16,10 +20,34 @@ class TestMovie {
 	@Autowired
 	MovieRepository repoMovie;
 	
+	@Autowired
+	EntityManager entityManager;
+	
 	@Test
-	void test() {
-		Movie movie = new Movie("Joker", 2019);
-		repoMovie.save(movie);
+	void testInsert() {
+		Movie joker = new Movie("Joker", 2019);
+		repoMovie.save(joker);
+		var id = joker.getIdMovie();
+		System.out.println("id new movie = " + id);
+		assertNotNull(id);
 	}
-
+	
+	@Test
+	void testSelectAll() {
+		//given
+		List<Movie> data = List.of(
+				new Movie("Joker", 2019),	
+				new Movie("The departed", 2006, 136),	
+				new Movie("The godfather", 1972, 220),	
+				new Movie("Apocalypse now", 1979),	
+				new Movie("Taxi driver", 1976),	
+				new Movie("Raging bull", 1980),	
+				new Movie("Interstellar", 2014)
+				);
+		data.forEach(entityManager::persist);
+		//when
+		var dataRead = repoMovie.findAll();
+		//then
+		dataRead.forEach(System.out::println);
+	}
 }
