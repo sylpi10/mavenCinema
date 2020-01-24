0f40c87b4b1b9753286deaf516b164fbdcf465d3
syl pi <syl.pillet@hotmail.fr>
2020-01-24 15:01:17 +0100

    SP/ first commit

---
+++ src/main/java/cinema/controller/PersonController.java
@@ -0,0 +1,11 @@
+package cinema.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import cinema.persistence.entity.Person;
+
+public class PersonController {
+	
+}
